19fe44159324310b811ab800f56483fbb9707aae
Taras Mandzak <tmandzak@gmail.com>
2016-09-19 19:03:40 +0200

    Add eval

--- Lesson3_RobotClass.py
+++ Lesson3_RobotClass.py
@@ -1,5 +1,3 @@
-# In this exercise, write a program that will
-# run your previous code twice.
 # Please only modify the indicated area below!
 
 from math import *
@@ -87,6 +85,16 @@ class robot:
         return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
 
 
+
+def eval(r, p):
+    sum = 0.0;
+    for i in range(len(p)): # calculate mean error
+        dx = (p[i].x - r.x + (world_size/2.0)) % world_size - (world_size/2.0)
+        dy = (p[i].y - r.y + (world_size/2.0)) % world_size - (world_size/2.0)
+        err = sqrt(dx * dx + dy * dy)
+        sum += err
+    return sum / float(len(p))
+
 #myrobot = robot()
 #myrobot.set_noise(5.0, 0.1, 5.0)
 #myrobot.set(30.0, 50.0, pi/2)
@@ -99,35 +107,40 @@ class robot:
 myrobot = robot()
 myrobot = myrobot.move(0.1, 5.0)
 Z = myrobot.sense()
-
 N = 1000
+T = 10 #Leave this as 10 for grading purposes.
+
 p = []
 for i in range(N):
-    x = robot()
-    x.set_noise(0.05, 0.05, 5.0)
-    p.append(x)
+    r = robot()
+    r.set_noise(0.05, 0.05, 5.0)
+    p.append(r)
 
-p2 = []
-for i in range(N):
-    p2.append(p[i].move(0.1, 5.0))
-p = p2
+for t in range(T):
+    myrobot = myrobot.move(0.1, 5.0)
+    Z = myrobot.sense()
 
-w = []
-for i in range(N):
-    w.append(p[i].measurement_prob(Z))
+    p2 = []
+    for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+    p = p2
 
-p3 = []
-index = int(random.random() * N)
-beta = 0.0
-mw = max(w)
-for i in range(N):
-    beta += random.random() * 2.0 * mw
-    while beta > w[index]:
-        beta -= w[index]
-        index = (index + 1) % N
-    p3.append(p[index])
-p = p3
+    w = []
+    for i in range(N):
+        w.append(p[i].measurement_prob(Z))
 
-print p #Leave this print statement for grading purposes!
+    p3 = []
+    index = int(random.random() * N)
+    beta = 0.0
+    mw = max(w)
+    for i in range(N):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % N
+        p3.append(p[index])
+    p = p3
+    #enter code here, make sure that you output 10 print statements.
+ 
 
 
