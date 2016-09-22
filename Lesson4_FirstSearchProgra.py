caf869cf71bfba11be387f1b193653344084a58a
Taras Mandzak <tmandzak@gmail.com>
2016-09-22 17:13:21 +0200

    + expand matrix

--- Lesson4_FirstSearchProgra.py
+++ Lesson4_FirstSearchProgra.py
@@ -1,25 +1,21 @@
-# ----------
+# -----------
 # User Instructions:
 # 
-# Define a function, search() that returns a list
-# in the form of [optimal path length, row, col]. For
-# the grid shown below, your function should output
-# [11, 4, 5].
+# Modify the function search so that it returns
+# a table of values called expand. This table
+# will keep track of which step each node was
+# expanded.
 #
-# If there is no valid path from the start point
-# to the goal, your function should return the string
-# 'fail'
+# Make sure that the initial cell in the grid 
+# you return has the value 0.
 # ----------
 
-# Grid format:
-#   0 = Navigable space
-#   1 = Occupied space
-
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
+
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
@@ -33,17 +29,22 @@ delta_name = ['^', '<', 'v', '>']
 
 def search(grid,init,goal,cost):
     closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]    
+    
     closed[init[0]][init[1]] = 1
+    
 
     x = init[0]
     y = init[1]
     g = 0
 
     open = [[g, x, y]]
+    expand[x][y] = 0
+    expand_id = 1
 
     found = False  # flag that is set when search is complete
     resign = False # flag set if we can't find expand
-    
+
     while not found and not resign:
         if len(open) == 0:
             resign = True
@@ -61,16 +62,13 @@ def search(grid,init,goal,cost):
                 for i in range(len(delta)):
                     x2 = x + delta[i][0]
                     y2 = y + delta[i][1]
-                    
                     if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
                             g2 = g + cost
                             open.append([g2, x2, y2])
+                            expand[x2][y2] = expand_id
+                            expand_id += 1
                             closed[x2][y2] = 1
-                            
-    if resign:
-        path = 'fail'
-    if found:
-        path = next
-    
-    return path
+    return expand
+
+print search(grid,init,goal,cost)
